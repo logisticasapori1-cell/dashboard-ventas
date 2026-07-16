f1bbe7e3cb0d79f88dc0be51e2009d90f0449dc4
logisticasapori1-cell <logisticasapori1@gmail.com>
2026-07-16 16:00:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ else:
         st.markdown("---")
         
         st.markdown("### 📌 Módulos de Operación")
+        Historico_Produccion_CREMIGURT = st.sidebar.file_uploader("📂 Cargar Histórico Producción CREMIGURT", type=["xlsx", "xls"])
         modulo_activo = st.radio(
             "Seleccione el área a visualizar:",
             ["1. Dashboard Venta Diaria & Forecast", "2. Control de Desviaciones (Mensual)", "3. Análisis Estratégico y Desviación vs Forecast"],
