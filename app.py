624011d1aef8f9552e68d8caf8375dd48d7279e4
logisticasapori1-cell <logisticasapori1@gmail.com>
2026-08-05 16:19:23 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ if not st.session_state['autenticado']:
 
     # Logo centrado y compacto
     if os.path.exists("logo_empresa.png"):
-        col_l, col_c, col_r = st.columns([1.2, 1.6, 1.2])
+        col_l, col_c, col_r = st.columns([0.8, 2.4, 0.8])
         with col_c:
             st.image("logo_empresa.png", use_container_width=True)
     else:
